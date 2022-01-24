223fb092b6f401ec1bab0ffd1b984d7468321427
Nico Evgl <nico.evangelista33@gmail.com>
2022-01-24 13:51:15 +0100

    generate multiple bubbles

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+/** generate one bubble
+ * const bubble = span
+ * bubble properties = Math.random() * 100
+ * const plusMinus = If Maths.random > 5 return 1, else return -1
+ * "--left" has to get negative or positive property
+ * setTimeout : remove bublle at the end of the animation (8s)
+ */
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-const size = Math.random() * 200 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-bubble.style.setProperty("--left", Math.random() * 100 + "%");
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 300);
